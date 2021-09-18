a8b5b4cdabaa3a760fd8f9d2f4a45a3ad05b0443
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-18 15:10:09 +0200

    Refactor archive

--- lib/k6/archive.ex
+++ lib/k6/archive.ex
@@ -1,21 +1,26 @@
 defmodule K6.Archive do
   def extract(archive_body, _archive_type, file_name) do
-    {:ok, archive} = :zip.zip_open(archive_body, [:memory])
-    {:ok, list_dir} = :zip.zip_list_dir(archive)
+    archive =
+      case :zip.zip_open(archive_body, [:memory]) do
+        {:ok, archive} -> archive
+        {:error, error} -> raise "Failed to open archive: #{inspect(error)}"
+      end
 
-    target_file =
-      list_dir
-      |> Enum.find_value(fn
-        {:zip_file, name, _info, _comment, _offset, _comp_size} ->
-          if Path.basename(to_string(name)) == file_name, do: name
-
-        _ ->
-          nil
-      end)
-
-    {:ok, {_file_name, content}} = :zip.zip_get(target_file, archive)
-    :zip.zip_close(archive)
-
-    {:ok, content}
+    with {:ok, list_dir} <- :zip.zip_list_dir(archive),
+         target_file <- Enum.find_value(list_dir, &match_file_name(file_name, &1)),
+         {:ok, {_file_name, content}} <- :zip.zip_get(target_file, archive) do
+      :zip.zip_close(archive)
+      {:ok, content}
+    else
+      error ->
+        :zip.zip_close(archive)
+        error
+    end
   end
+
+  defp match_file_name(file_name, {:zip_file, name, _info, _comment, _offset, _comp_size}) do
+    if Path.basename(to_string(name)) == file_name, do: name
+  end
+
+  defp match_file_name(_, _), do: nil
 end
