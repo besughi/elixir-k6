538f539eb1aad2798a16c3fcb95b02f27d8d043c
danielefongo <danielefongo@gmail.com>
2021-09-18 11:31:29 +0200

    Download release depending on target

--- lib/mix/tasks/k6.install.ex
+++ lib/mix/tasks/k6.install.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.K6.Install do
   use Mix.Task
 
-  @binary_url "https://github.com/grafana/k6/releases/download/v0.34.1/k6-v0.34.1-macos-amd64.zip"
+  @binary_path Path.join(Path.dirname(Mix.Project.build_path()), "k6")
 
   @shortdoc "Installs K6 on the local machine"
   def run(_) do
@@ -9,19 +9,33 @@ defmodule Mix.Tasks.K6.Install do
     {:ok, _} = Application.ensure_all_started(:ssl)
 
     body =
-      case :httpc.request(:get, {@binary_url, []}, [], body_format: :binary) do
+      case :httpc.request(:get, {binary_url(), []}, [], body_format: :binary) do
         {:ok, {{_, 200, _}, _headers, body}} ->
           body
 
         other ->
-          raise "couldn't fetch #{@binary_url}: #{inspect(other)}"
+          raise "couldn't fetch #{binary_url()}: #{inspect(other)}"
       end
 
     {:ok, archive} = :zip.zip_open(body, [:memory])
     {:ok, {_file_name, content}} = :zip.zip_get(~c'k6-v0.34.1-macos-amd64/k6', archive)
     :zip.zip_close(archive)
 
-    File.write!("/tmp/xyz", content)
-    File.chmod!("/tmp/xyz", 0o755)
+    File.write!(@binary_path, content)
+    File.chmod!(@binary_path, 0o755)
   end
+
+  defp binary_url, do: "https://github.com/grafana/k6/releases/download/#{version()}/#{target()}"
+
+  defp target do
+    case :os.type() do
+      {:unix, :darwin} ->
+        "k6-#{version()}-macos-amd64.zip"
+
+      other ->
+        raise "Not implemented for #{inspect(other)}"
+    end
+  end
+
+  defp version, do: "v0.34.1"
 end
