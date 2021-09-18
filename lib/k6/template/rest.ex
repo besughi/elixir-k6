c0c1725b48e95a63d8bcc0c9be4f389bf8cd2f9d
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-18 18:43:43 +0200

    Correct typo

--- lib/k6/template/rest.ex
+++ lib/k6/template/rest.ex
@@ -7,10 +7,10 @@ defmodule K6.Template.Rest do
     url = Keyword.get(opts, :url, Template.default_base_url())
 
     create_directory(Path.dirname(filename))
-    create_file(filename, graphql_template(url: url))
+    create_file(filename, rest_template(url: url))
   end
 
-  embed_template(:graphql, """
+  embed_template(:rest, """
   import http from 'k6/http';
   import {check, sleep} from 'k6';
 
