7eac3e9033cc0183c95aab7e7b7fe1d1c1f92a1b
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-18 17:21:19 +0200

    Handle type of test when generating tests

--- lib/mix/tasks/k6.gen.test.ex
+++ lib/mix/tasks/k6.gen.test.ex
@@ -1,16 +1,26 @@
 defmodule Mix.Tasks.K6.Gen.Test do
   use Mix.Task
 
+  alias K6.Template
+
+  @switches [
+    type: :string
+  ]
+
   @shortdoc "Generates a new k6 test"
   def run(args) do
-    test_name =
-      case OptionParser.parse!(args, strict: [], aliases: []) do
-        {_switches, [test_name]} -> test_name
-        {_switches, _positional_args} -> raise "Please provide a single name for your test."
-      end
+    case OptionParser.parse!(args, strict: @switches, aliases: []) do
+      {switches, [test_name]} ->
+        type = Keyword.get(switches, :type, "rest")
+        filename = Path.join(["priv", "k6", test_name <> ".js"])
 
-    filename = Path.join(["priv", "k6", test_name <> ".js"])
+        do_generate(type, filename, [])
 
-    K6.Template.Graphql.generate(filename, url: "http://localhost:4000/graphql")
+      {_switches, _positional_args} ->
+        raise "Please provide a single name for your test."
+    end
   end
+
+  defp do_generate("rest", filename, opts), do: Template.Rest.generate(filename, opts)
+  defp do_generate("graphql", filename, opts), do: Template.Graphql.generate(filename, opts)
 end
