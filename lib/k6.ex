ec6446c47b39a87aef974462c4615a8a5a465555
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-19 16:51:19 +0200

    Remove dead code

--- lib/k6.ex
+++ lib/k6.ex
@@ -1,18 +1,2 @@
 defmodule K6 do
-  @moduledoc """
-  Documentation for `K6`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> K6.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
