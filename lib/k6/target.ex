cbf1b723a223c87832e839286b78d179ab82ac01
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-19 16:51:00 +0200

    Add support for downloading arm builds of k6

--- lib/k6/target.ex
+++ lib/k6/target.ex
@@ -8,16 +8,37 @@ defmodule K6.Target do
   Get k6 target version
   """
   @spec get!(String.t()) :: {file_type(), String.t()}
-  def get!(version, os_type \\ &:os.type/0) do
-    case os_type.() do
-      {:unix, :darwin} ->
+  def get!(version, platform \\ &platform/0) do
+    case platform.() do
+      {:x86, :darwin} ->
         {:zip, "k6-#{version}-macos-amd64.zip"}
 
-      {:unix, :linux} ->
+      {:x86, :linux} ->
         {:tar_gz, "k6-#{version}-linux-amd64.tar.gz"}
+
+      {:arm, :darwin} ->
+        {:zip, "k6-#{version}-macos-arm64.zip"}
+
+      {:arm, :linux} ->
+        {:tar_gz, "k6-#{version}-linux-arm64.tar.gz"}
 
       other ->
         raise "Not implemented for #{inspect(other)}"
     end
+  end
+
+  defp platform do
+    case {architecture(), :os.type()} do
+      {'x86_64', {:unix, :darwin}} -> {:x86, :darwin}
+      {'arm', {:unix, :darwin}} -> {:arm, :darwin}
+      {'x86_64', {:unix, :linux}} -> {:x86, :linux}
+      {'arm', {:unix, :linux}} -> {:arm, :linux}
+    end
+  end
+
+  defp architecture do
+    arch_str = :erlang.system_info(:system_architecture)
+    [arch | _] = :string.split(arch_str, '-')
+    arch
   end
 end
--- test/target_test.exs
+++ test/target_test.exs
@@ -3,19 +3,26 @@ defmodule K6.TargetTest do
 
   alias K6.Target
 
-  test "returns k6 target for macos" do
-    os_type = fn ->
-      {:unix, :darwin}
-    end
+  test "returns k6 target for macos - x86" do
+    os_type = fn -> {:x86, :darwin} end
 
     assert {:zip, "k6-v0.34.1-macos-amd64.zip"} = Target.get!("v0.34.1", os_type)
   end
 
-  test "returns k6 target for linux" do
-    os_type = fn ->
-      {:unix, :linux}
-    end
+  test "returns k6 target for linux - x86" do
+    os_type = fn -> {:x86, :linux} end
 
     assert {:tar_gz, "k6-v0.34.1-linux-amd64.tar.gz"} = Target.get!("v0.34.1", os_type)
+  end
+
+  test "returns k6 target for macos - arm" do
+    os_type = fn -> {:arm, :darwin} end
+    assert {:zip, "k6-v0.34.1-macos-arm64.zip"} = Target.get!("v0.34.1", os_type)
+  end
+
+  test "returns k6 target for linux - arm" do
+    os_type = fn -> {:arm, :linux} end
+
+    assert {:tar_gz, "k6-v0.34.1-linux-arm64.tar.gz"} = Target.get!("v0.34.1", os_type)
   end
 end
