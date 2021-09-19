4c17ea64293e0076cf1d22df5080b97fbbf8cb69
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2021-09-19 18:21:18 +0200

    Verify checksum on download when possible

--- lib/k6/downloader.ex
+++ lib/k6/downloader.ex
@@ -4,6 +4,17 @@ defmodule K6.Downloader do
   """
   alias K6.Target
 
+  @known_checksums %{
+    "k6-v0.34.1-linux-amd64.tar.gz" =>
+      "932668f86b4f5cfd4d33cd7c05651f9638570ccf303480ab5c207cba394e1732",
+    "k6-v0.34.1-linux-arm64.tar.gz" =>
+      "3f18dec2433c65cdc1bc1ddeafeabe77e9be00443987f5ec85e60c1504a13144",
+    "k6-v0.34.1-macos-amd64.zip" =>
+      "b852687554455dd98b8a3c193608503263e87899be18e337e7473ae656c273f2",
+    "k6-v0.34.1-macos-arm64.zip" =>
+      "211fdce7f33df0643e0aaca59af9550b8c999cb6d4bbd6b741db059ecf9d7d73"
+  }
+
   @doc """
   Downloads k6 package
   """
@@ -27,6 +38,7 @@ defmodule K6.Downloader do
 
     case :httpc.request(:get, {binary_url(k6_version, target), []}, options, body_format: :binary) do
       {:ok, {{_, 200, _}, _headers, body}} ->
+        verify_checksum!(body, target)
         {file_type, body}
 
       other ->
@@ -37,4 +49,13 @@ defmodule K6.Downloader do
   defp binary_url(k6_version, target) do
     "https://github.com/grafana/k6/releases/download/#{k6_version}/#{target}"
   end
+
+  def verify_checksum!(body, target) do
+    expected_checksum = Map.get(@known_checksums, target)
+
+    if expected_checksum != nil and String.downcase(checksum(body)) != expected_checksum,
+      do: raise("Download error: checksum verification failed!")
+  end
+
+  defp checksum(body), do: :sha256 |> :crypto.hash(body) |> Base.encode16()
 end
